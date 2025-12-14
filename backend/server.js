1ddffae5582687cd9d2221942a79c460341bb7ad
Subhajit Paramanik <paramaniksubhajit2007@gmail.com>
2025-12-14 15:41:35 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,12 @@ const mysql = require("mysql2");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://subhajit2425.github.io",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // MySQL Connection
