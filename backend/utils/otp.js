44e839e592e011964cf693e0e13de577d6e154f1
Subhajit Paramanik <paramaniksubhajit2007@gmail.com>
2025-12-21 15:54:46 +0530

    Fix

--- backend/utils/otp.js
+++ backend/utils/otp.js
@@ -1,2 +1,5 @@
-exports.generateOTP = () =>
-  Math.floor(100000 + Math.random() * 900000).toString();
+const generateOTP = () => {
+  return Math.floor(100000 + Math.random() * 900000).toString();
+};
+
+module.exports = generateOTP;
